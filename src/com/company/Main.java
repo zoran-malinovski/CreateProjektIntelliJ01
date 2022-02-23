7ae855cce35b2f2bf55c3c8fb225524823e9de79
Zoran <z.malinovski79@gmail.com>
2022-02-23 07:55:02 +0100

    Added first line of code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //added first lines of code
+        String model;
+        String color;
+        boolean engineState = false;
+
     }
 }
