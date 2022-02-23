0b7b7c730c89faf42a4843384d236b275b58dd58
Zoran <z.malinovski79@gmail.com>
2022-02-23 21:47:45 +0100

    Brench-04

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,30 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //added first lines of code
-        String model;
-        String color;
-        boolean engineState = false;
 
-        void startEngine () {
-
-            if (engineState == true)
-                System.out.println("Engine is already ON");
-            else {
-                engineState = true;
-                System.out.println("Engine is OFF");
-            }
-        }
-
-        void showAtribute(){
-            System.out.println("This motorcycle is a " + color + " " + model);
-            if(engineState == true)
-                System.out.println("The engine is on");
-            else
-                System.out.println("The Engine is off");
-
-        }
 
     }
-
 }
